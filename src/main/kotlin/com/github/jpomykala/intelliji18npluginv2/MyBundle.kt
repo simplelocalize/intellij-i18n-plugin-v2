3612c93ad9cb0aca4c67b4535ebdeb7d217a06b7
GitHub Action <action@github.com>
2021-02-03 16:11:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/jpomykala/intelliji18npluginv2/MyBundle.kt
+++ src/main/kotlin/com/github/jpomykala/intelliji18npluginv2/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jpomykala.intelliji18npluginv2
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/jpomykala/intelliji18npluginv2/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jpomykala.intelliji18npluginv2.services
+
+import com.github.jpomykala.intelliji18npluginv2.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
